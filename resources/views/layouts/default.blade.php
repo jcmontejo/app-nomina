84681c5aa08b1ced875bacd4f8f34c4678fcb8d3
Juan Carlos Montejo <jncrlsmontejo@gmail.com>
2022-05-24 22:51:43 -0500

    Update default.blade.php

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -435,7 +435,7 @@
                         <!-- Search form -->
                         <form class="navbar-search form-inline" id="navbar-search-main">
                             <div class="input-group input-group-merge search-bar">
-                                <span class="input-group-text" id="topbar-addon">
+                                {{-- <span class="input-group-text" id="topbar-addon">
                                     <svg class="icon icon-xs" x-description="Heroicon name: solid/search"
                                         xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
                                         aria-hidden="true">
@@ -443,16 +443,16 @@
                                             d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
                                             clip-rule="evenodd"></path>
                                     </svg>
-                                </span>
-                                <input type="text" class="form-control" id="topbarInputIconLeft" placeholder="Buscar"
-                                    aria-label="Search" aria-describedby="topbar-addon">
+                                </span> --}}
+                                {{-- <input type="text" class="form-control" id="topbarInputIconLeft" placeholder="Buscar"
+                                    aria-label="Search" aria-describedby="topbar-addon"> --}}
                             </div>
                         </form>
                         <!-- / Search form -->
                     </div>
                     <!-- Navbar links -->
                     <ul class="navbar-nav align-items-center">
-                        <li class="nav-item dropdown">
+                        {{-- <li class="nav-item dropdown">
                             <a class="nav-link text-dark notification-bell unread dropdown-toggle"
                                 data-unread-notifications="true" href="#" role="button" data-bs-toggle="dropdown"
                                 data-bs-display="static" aria-expanded="false">
@@ -589,7 +589,7 @@
                                     </a>
                                 </div>
                             </div>
-                        </li>
+                        </li> --}}
                         <li class="nav-item dropdown ms-lg-3">
                             <a class="nav-link dropdown-toggle pt-1 px-0" href="#" role="button"
                                 data-bs-toggle="dropdown" aria-expanded="false">
@@ -656,7 +656,7 @@
             </div>
         </nav>
         @yield('content')
-        <footer class="bg-white rounded shadow p-5 mb-4 mt-4">
+        {{-- <footer class="bg-white rounded shadow p-5 mb-4 mt-4">
             <div class="row">
                 <div class="col-12 col-md-4 col-xl-6 mb-4 mb-md-0">
                     <p class="mb-0 text-center text-lg-start">© 2022-<span class="current-year"></span> <a
@@ -666,7 +666,7 @@
                 <div class="col-12 col-md-8 col-xl-6 text-center text-lg-start">
                 </div>
             </div>
-        </footer>
+        </footer> --}}
     </main>
     <!-- Core -->
     <script src="{{ asset('/vendor/@popperjs/core/dist/umd/popper.min.js') }}"></script>
